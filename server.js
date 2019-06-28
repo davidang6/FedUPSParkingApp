344f3af7aa5dbe450ba7efad1cd09b4427d5c252
davidang6 <cdang1024@gmail.com>
2019-06-27 22:56:06 -0400

    separate js attempt

--- server.js
+++ server.js
@@ -4,30 +4,25 @@ var fs = require('fs');
 // var iothub = require('azure-iothub');
 var port = process.env.PORT || 1337;
 
-//var connectionString = 'HostName=FedUPS.azure-devices.net;SharedAccessKeyName=FedUPS-iot-hub;SharedAccessKey=aA06gd7aqFKjbG9xOshttDRI36eh1fBcpgNLwtFc1eY=';
-
-//var registry = iothub.Registry.fromConnectionString(connectionString);
-
 /*
-var device = {
-    deviceId: 'sample-device-' + Date.now()
-};
-*/
+const express = require('express');
+const app = express();
 
-/*
+const parkingInfo = [];
 
-pubsub.getSubscriptions(function(err, subscriptions){
-    subscriptions.forEach(subscription => console.log(subscription.name));
-})
+app.get('/parkingInfo', (req, res) => {
+    res.send("Hello World!");
+});
+
+app.listen(port);
 */
 
 http.createServer(function (req, res) {
-    fs.readFile("Page1.html", function (err, data) {
+    fs.readFile('Page1.html', function(err, data){
         res.writeHead(200, { 'Content-Type': 'text/html' });
         res.write(data);
-        res.write(Date());
         res.end();
-    });
+    })
 }).listen(port, () => {
     console.log("running on port" + port);
 });

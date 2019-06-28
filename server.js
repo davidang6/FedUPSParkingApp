a414c12a17775fdb6056c8d2fa2ea9af1f3e2bea
davidang6 <cdang1024@gmail.com>
2019-06-27 20:40:37 -0400

    added test jQuery

--- server.js
+++ server.js
@@ -21,38 +21,12 @@ pubsub.getSubscriptions(function(err, subscriptions){
 })
 */
 
-const {PubSub} = require('@google-cloud/pubsub');
-const pubsub = new PubSub();
-
-pubsub
-  .subscription('parking_sub')
-  .on(`message`, message => {
-    try {
-      const msg = JSON.parse(message.data);
-      console.log("Thing received");
-      const deviceId = msg.deviceID;
-      console.log("Received message with status: " + msg.status);
-      var time = new Date(0);
-      time.setUTCSeconds(msg.time);
-      console.log("Received message with time: " + time);
-      message.ack();
-    } catch (err) {
-      console.log("Error ocurred while processing message: ${err}");
-    }
-  })
-  .on(`error`, err => {
-    console.log("Error ocurred while receiving message: ${err}");
-  })
-  .on(`close`, () => {
-    console.log("subscription closed unexpectedly");
-  })
-
 http.createServer(function (req, res) {
     fs.readFile("Page1.html", function (err, data) {
         res.writeHead(200, { 'Content-Type': 'text/html' });
         res.write(data);
         res.write(Date());
-        res.end('Hello World\n');
+        res.end();
     });
 }).listen(port, () => {
     console.log("running on port" + port);
